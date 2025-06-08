efc191d241fc88e44b7f643eed52466c163c30d7
David Suarez <gcvanguardia@gmail.com>
2025-06-08 10:52:29 -0500

    feat: add excel download feture

--- src/app/pages/listado/listado.page.ts
+++ src/app/pages/listado/listado.page.ts
@@ -5,13 +5,17 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonRefresher, IonRefresher
 import { TicketsService, SoldTicket } from '../../services/tickets/tickets.service';
 import { AuthService } from "../../services/auth/auth.service";
 import { NavController } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { cloudDownloadOutline } from 'ionicons/icons';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.page.html',
   styleUrls: ['./listado.page.scss'],
   standalone: true,
-  imports: [ IonFooter, IonBackButton, IonButtons, IonSpinner, IonItem, IonRefresherContent, IonRefresher, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput ]
+  imports: [IonFooter, IonBackButton, IonButtons, IonSpinner, IonItem, IonRefresherContent, IonRefresher, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonButton, IonIcon]
 })
 export class ListadoPage implements OnInit {
   soldTickets: SoldTicket[] = [];
@@ -24,7 +28,9 @@ export class ListadoPage implements OnInit {
     private ticketSvc: TicketsService,
     private auth: AuthService,
     private nav: NavController,
-  ) { }
+  ) { 
+    addIcons({ cloudDownloadOutline });
+  }
 
   ngOnInit() {
     this.getUser();
@@ -73,6 +79,29 @@ export class ListadoPage implements OnInit {
 
   get total(): number {
     return this.filteredTickets.reduce((sum, t) => sum + (Number(t.ticket_price) || 0), 0);
+  }
+
+  exportToExcel() {
+    const data = this.filteredTickets.map(t => ({
+      Número: t.number,
+      Método: t.payment_method,
+      Comprador: t.buyer_name,
+      CC: t.buyer_cc,
+      Email: t.buyer_email,
+      Teléfono: t.buyer_phone,
+      Vendedor: t.seller_name,
+      'Email Vendedor': t.seller_email,
+      Fecha: t.sold_at,
+      Valor: t.ticket_price
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Listado');
+
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(blob, 'listado_boletas_vendidas_equipo_luna.xlsx');
   }
 
 }
